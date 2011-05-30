1c9e7b75434f54b89d2cc5bce69fe4b3dac1079d
John St. Clair <stclairjohn@gmail.com>
2011-05-30 10:16:04 +0200

    Coverted vbs to powershell

---
+++ msmq_install.ps1
@@ -0,0 +1,21 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Messaging")
+Function Delete-Queue($queueName) {
+    if ([System.Messaging.MessageQueue]::Exists($queueName))
+    {
+        [System.Messaging.MessageQueue]::Delete($queueName)
+        Write-Host "Deleting queue $queueName"
+    }
+}
+
+Function Create-Queue($queueName, $userName) {
+    $qb = [System.Messaging.MessageQueue]::Create($queueName)
+	 if ($userName) {
+	    $acl = ([System.Messaging.MessageQueueAccessRights]::GenericRead -bor [System.Messaging.MessageQueueAccessRights]::GenericWrite)
+		 $qb.SetPermissions($userName, $acl, [System.Messaging.AccessControlEntryType]::Set)
+	 }
+}
+
+$queues = ".\private$\messagebus", ".\private$\subscriptions", ".\private$\error", ".\private$\client", ".\private$\worker", ".\private$\worker2", ".\private$\external"
+$appPoolUser = "DefaultAppPool" # Win7/2008 + IIS 7.5
+
+$queues | % { Delete-Queue $_; Create-Queue $_ $appPoolUser }
